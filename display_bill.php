be252981a9538e2dcb822831214ced51fadbb47f
jenn <epistemicpolymathic5@gmail.com>
2017-01-30 00:51:43 -0500

    Updated Bill Claculation with some formatting updates and such.

--- display_bill.php
+++ display_bill.php
@@ -19,29 +19,26 @@ $water_Quan = $_POST['water_Quan'];
 #Declare Variables to Calculate Price and Budget
 
 $apple_Price = $apple_Quan * 1.00;
-$banana_Price = $banana_Quan * 0.5;
-$milk_Price = $milk_Quan * 2.8;
+$banana_Price = $banana_Quan * 0.50;
+$milk_Price = $milk_Quan * 2.80;
 $cake_Price = $cake_Quan * 10.00;
-$toast_Price = $toast_Quan * 1.6;
-$cheese_Price = $cheese_Quan * 4.5;
+$toast_Price = $toast_Quan * 1.60;
+$cheese_Price = $cheese_Quan * 4.50;
 $cookie_Price = $cookie_Quan * 5.00;
 $water_Price = $water_Quan * 1.00;
 $total_Price = $apple_Price + $banana_Price + $milk_Price + $cake_Price + $toast_Price + $cheese_Price + $cookie_Price
     + $water_Price;
 
 
-
-
-
 # Some more necessary Variable Declaration
 
 
 #Tax Calculation
-if ($total_Price > 10){
+if ($total_Price > 10) {
 
     $tax = .015;
 
-} else{
+} else {
 
     $tax = .02;
 
@@ -53,10 +50,9 @@ $total_Amount = ($total_Price * 1.0) + $tax;
 
 #Did it exceed Budget Test
 
-if ($total_Amount > $userBudget){
+if ($total_Amount > $userBudget) {
 
     $message = "You have exceeded your budget $userBudget, review your order!";
-
 
 
 } else {
@@ -88,11 +84,21 @@ if ($total_Amount > $userBudget){
 
     }
 
-    .water_img{
+    .water_img {
 
         height: 128px;
         width: 128px;
 
+    }
+
+    .font_bold {
+
+        font-weight: bold;
+    }
+
+    .font_color {
+
+        font-weight: bold;
     }
 
 </style>
@@ -111,63 +117,63 @@ if ($total_Amount > $userBudget){
 
         <td><img src="img/apple.png"></td>
         <td><?= $apple_Quan; ?></td>
-        <td><?= $apple_Price; ?></td>
+        <td><?= "$" . $apple_Price; ?></td>
     </tr>
 
     <tr>
 
         <td><img src="img/banana.png"></td>
         <td><?= $banana_Quan; ?></td>
-        <td><?= $banana_Price; ?></td>
+        <td><?= "$" . $banana_Price; ?></td>
     </tr>
 
     <tr>
 
         <td><img src="img/milk.png"></td>
         <td><?= $milk_Quan; ?></td>
-        <td><?= $milk_Price; ?></td>
+        <td><?= "$" . $milk_Price; ?></td>
     </tr>
 
     <tr>
 
         <td><img src="img/cake.png"></td>
         <td><?= $cake_Quan; ?></td>
-        <td><?= $cake_Price; ?></td>
+        <td><?= "$" . $cake_Price; ?></td>
     </tr>
 
     <tr>
 
         <td><img src="img/toast.png"></td>
         <td><?= $toast_Quan; ?></td>
-        <td><?= $toast_Price; ?></td>
+        <td><?= "$" . $toast_Price; ?></td>
     </tr>
 
     <tr>
 
         <td><img src="img/cheese.png"></td>
         <td><?= $cheese_Quan; ?></td>
-        <td><?= $cheese_Price; ?></td>
+        <td><?= "$" . $cheese_Price; ?></td>
     </tr>
 
     <tr>
 
         <td><img src="img/cookie.png"></td>
         <td><?= $cookie_Quan; ?></td>
-        <td><?= $cookie_Price; ?></td>
+        <td><?= "$" . $cookie_Price; ?></td>
     </tr>
 
     <tr>
 
         <td><img src="img/water.jpg" class="water_img"></td>
         <td><?= $water_Quan; ?></td>
-        <td><?= $water_Price; ?></td>
+        <td><?= "$" . $water_Price; ?></td>
     </tr>
 
 
     <tr>
 
         <td class="text_style">Total</td>
-        <td colspan="2"><?= $total_Price; ?></td>
+        <td colspan="2"><?= "$" . $total_Price; ?></td>
 
     </tr>
 
@@ -177,9 +183,9 @@ if ($total_Amount > $userBudget){
 <p>
 
     <!-- Budget testing output goes here -->
-    <span class="tax">Tax = <?= $tax; ?></span> <br>
-    <span class="font_color">Total Amount = <?= $total_Amount ?></span> <br>
-    <?= $message; ?>
+    <span class="font_bold">Tax = <?= $tax; ?></span> <br> <br>
+    <span class="font_bold">Total Amount = <?= $total_Amount ?></span> <br> <br>
+    <span class="font_color"> <?= $message; ?></span>
 
 </p>
 
