49a6594643dd9994d06445e1612b19b58ac96c03
jenn <epistemicpolymathic5@gmail.com>
2017-01-29 23:59:20 -0500

    Basic Outline for PHP file complete

--- display_bill.php
+++ display_bill.php
@@ -1,7 +1,127 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: EpistemicPolymath
- * Date: 1/29/2017
- * Time: 4:20 PM
+ *HW 02 Grocery Shopping Tracker
+ *800768070_HW02
+ *Jennifer Tillman
  */
+
+#Declare variables taken from HTML
+$userBudget = $_POST['userBudget'];
+$apple_Quan = $_POST['apple_Quan'];
+$banana_Quan = $_POST['banana_Quan'];
+$milk_Quan = $_POST['milk_Quan'];
+$cake_Quan = $_POST['cake_Quan'];
+$toast_Quan = $_POST['toast_Quan'];
+$cheese_Quan = $_POST['cheese_Quan'];
+$cookie_Quan = $_POST['cookie_Quan'];
+$water_Quan = $_POST['water_Quan'];
+
+#Declare Variables to Calculate Price and Budget
+
+$apple_Price = " ";
+$banana_Price = " ";
+$milk_Price = " ";
+$cake_Price = " ";
+$toast_Price = " ";
+$cheese_Price = " ";
+$cookie_Price = " ";
+$water_Price = " ";
+$total_Price = " ";
+
+
+
+?>
+
+<style>
+
+    .phpTable {
+
+        border: double;
+
+    }
+
+    td, th {
+
+        border-style: double;
+        font-weight: bold;
+
+    }
+
+    th {
+
+        font-weight: bold;
+
+    }
+
+</style>
+
+<table class="phpTable">
+
+    <tr>
+
+        <th>Item</th>
+        <th>Quantity</th>
+        <th>Price</th>
+
+    </tr>
+
+    <tr>
+
+        <td><img src="img/apple.png"></td>
+        <td><?= $apple_Quan; ?></td>
+        <td><?= $apple_Price; ?></td>
+    </tr>
+
+    <tr>
+
+        <td><img src="img/banana.png"></td>
+        <td><?= $banana_Quan; ?></td>
+        <td><?= $banana_Price; ?></td>
+    </tr>
+
+    <tr>
+
+        <td><img src="img/milk.png"></td>
+        <td><?= $milk_Quan; ?></td>
+        <td><?= $milk_Price; ?></td>
+    </tr>
+
+    <tr>
+
+        <td><img src="img/cake.png"></td>
+        <td><?= $cake_Quan; ?></td>
+        <td><?= $cake_Price; ?></td>
+    </tr>
+
+    <tr>
+
+        <td><img src="img/toast.png"></td>
+        <td><?= $toast_Quan; ?></td>
+        <td><?= $toast_Price; ?></td>
+    </tr>
+
+    <tr>
+
+        <td><img src="img/cheese.png"></td>
+        <td><?= $cheese_Quan; ?></td>
+        <td><?= $cheese_Price; ?></td>
+    </tr>
+
+    <tr>
+
+        <td><img src="img/cookie.png"></td>
+        <td><?= $cookie_Quan; ?></td>
+        <td><?= $cookie_Price; ?></td>
+    </tr>
+
+    <tr>
+
+        <td><img src="img/water.png"></td>
+        <td><?= $water_Quan; ?></td>
+        <td><?= $water_Price; ?></td>
+    </tr>
+
+
+</table>
+
+
